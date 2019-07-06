2eb543ea4b65ef09cd366e93a08e8a5b480966a4
KaynAssassin <allmid66@gmail.com>
2019-07-06 15:28:02 +0700

    update code

--- crawler/spiders/xwatch.py
+++ crawler/spiders/xwatch.py
@@ -14,7 +14,6 @@ class XwatchSpider(scrapy.Spider):
 
         names = response.xpath("//div[@class = 'frame_inner']/h2/a[@class = 'name']/text()").getall()
         prices = response.xpath("//div[@class = 'price_arae']/div[@class = 'price_current']/text()").getall()
-        # links = response.xpath("//div[@class = 'item']/div[@class = 'frame_inner']").css("img::attr(src)").getall()
         links_first = response.xpath("//div[@class = 'frame_inner']").css("a img::attr(src)").getall()
         links_second = response.xpath("//div[@class = 'frame_inner']").css("a img::attr(data-src)").getall()
         list_name = []
@@ -28,25 +27,19 @@ class XwatchSpider(scrapy.Spider):
         # pdb.set_trace()
 
         for name in names:
-            index = 0
-            s = ""
-            while index < len(name):
-                if (65 <= ord(name[index]) <= 90) or (97 <= ord(name[index]) <= 122) or (48 <= ord(name[index]) <= 57) :
-                    s = s + name[index]
-                index = index + 1
-            if s != "":
-                list_name.append(s)
-
+            name = "".join(name)
+            name = name.strip()
+            if name != "":
+                list_name.append(name)
+        
         for price in prices:
-            index = 0
-            s = ""
-            while index < len(price):
-                if (65 <= ord(price[index]) <= 90) or (97 <= ord(price[index]) <= 122) or (48 <= ord(price[index]) <= 57) :
-                    s = s + price[index]
-                index = index + 1
-            if s != "":
-                list_price.append(s)
-                 
+            price = "".join(price)
+            price = price.strip()
+            price = price.split("\\")[0]
+            if price != "":
+                list_price.append(price)
+
+
         i = 0
         while i < len(list_link):
             yield{  'link' : list_link[i],
